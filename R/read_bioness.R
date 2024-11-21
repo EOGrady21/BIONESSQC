56d32036bc186acb9f58f8422b48cd1d451eab82
EOGrady21 <38440373+EOGrady21@users.noreply.github.com>
2024-11-21 12:55:35 -0400

    INITIAL COMMIT

---
+++ R/read_bioness.R
@@ -0,0 +1,91 @@
+# READ BIONESS electronic files
+
+#' Read BIONESS electronic files
+#' Returns a BIONESS class list object with metadata represented as a list and
+#' data represented as a dataframe.
+#'
+#' @param file
+#'
+#' @return a BIONESS class object with a /{metadata} and /{data} section
+#' @export
+
+read_bioness <- function(file) {
+  require(tidyverse)
+  require(fs)
+
+  # tidy path and check inputs
+  file <- fs::path_tidy(file)
+  shortname <- str_split_i(file, pattern = '/',  i = -1)
+  file_type <- NULL
+  if (length(grep(shortname, pattern = 'T14')) > 0) {
+    file_type <- 'T'
+    #message('File type .T14 detected!')
+  }
+  if (length(grep(shortname, pattern = 'B14')) > 0) {
+    file_type <- 'B'
+    #message('File type .B14 detected!')
+  }
+  if(is.null(file_type)) {
+    stop('File type not recognized!')
+  }
+
+  if (file_type == 'B') {
+    stop('File type not supported! IN DEVELOPMENT')
+  }
+
+  # Read T14 files
+  if (file_type == 'T') {
+    linedata <- readLines(file)
+  # grab net header
+    netinfohead <- which(linedata == 'Net Information')
+    if (length(netinfohead) == 0 | length(netinfohead) > 1) {
+      stop('Net Information header not properly detected!')
+    }
+    # grab tow metadata header
+    towmetahead <- which(linedata == 'Tow Configuration Information')
+    if (length(towmetahead) == 0 | length(towmetahead) > 1) {
+      stop('Tow Metadata header not properly detected!')
+    }
+
+    # gather lines into sections
+    netinfodata <- linedata[(netinfohead+1):(towmetahead-1)]
+    towmetadata <- linedata[(towmetahead+1):length(linedata)]
+
+    # tidy each section
+    sectionsplitind <- grep(netinfodata, pattern = '===')
+    netinfodata <- netinfodata[-sectionsplitind]
+    emptylineind <- which(netinfodata == "")
+    if (length(emptylineind) > 0) {
+      netinfodata <- netinfodata[-emptylineind]
+    }
+
+    sectionsplitind <- grep(towmetadata, pattern = '===')
+    towmetadata <- towmetadata[-sectionsplitind]
+    emptylineind <- which(towmetadata == "")
+    if (length(emptylineind) > 0) {
+      towmetadata <- towmetadata[-emptylineind]
+    }
+
+    # format into bioness object
+
+    data <- list('metadata'= list(), 'data' = data.frame())
+    class(data) <- 'BIONESS'
+
+    towmetadata_names <- str_trim(str_split_i(towmetadata, pattern = '=', i = 1), side = 'both')
+    towmetadata_values <- str_trim(str_split_i(towmetadata, pattern = '=', i = 2), side = 'both')
+
+    data$metadata <- as.list(towmetadata_values)
+    names(data$metadata) <- towmetadata_names
+
+    netdataframe <- str_split(netdinfodata, pattern = ",")
+    netdataframe <- as.data.frame(netdataframe,
+                                  row.names = c('net_number', 'time', 'depth', 'volume', 'temperature', 'salinity' ,'strobe'))
+    names(netdataframe) <- NA
+
+    data$data <- data.frame(t(netdataframe))
+
+    return(data)
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+
+# BIONESSQC
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of BIONESSQC is to ...
+
+## Installation
+
+You can install the development version of BIONESSQC like so:
+
+``` r
+install_github('EOGrady21/BIONESSQC')
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(BIONESSQC)
+## basic example code
+```
+
